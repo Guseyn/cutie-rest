7dba60a71f306f7f5d12915260c0735e40841c2b
gismayilov <gismayilov@griddynamics.com>
2018-04-06 12:37:00 +0300

    request body

---
+++ src/backend/RequestWithEndEvent.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const AsyncObject = require('@guseyn/cutie').AsyncObject;
+
+// Represented result is request
+class RequestWithEndEvent extends AsyncObject {
+
+  constructor(request, event) {
+    super(request, event);
+  }
+
+  definedAsyncCall() {
+    return (request, event) => {
+      request.on('end', event);
+      return request;
+    }
+  }
+
+}
+
+module.exports = RequestWithEndEvent;
