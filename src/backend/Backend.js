b95e14db8d5b651468a562f8e09e81b46502c473
gismayilov <gismayilov@griddynamics.com>
2018-09-14 17:01:11 +0300

    Make Backend AsyncObject

--- src/backend/Backend.js
+++ src/backend/Backend.js
@@ -1,6 +1,8 @@
 'use strict'
 
-const Event = require('@cuties/cutie').Event;
+const {
+  AsyncObject, Event
+} = require('@cuties/cutie');
 const {
   ListeningServer,
   CreatedHttpServer,
@@ -10,18 +12,23 @@ const RestApi = require('./api/RestApi');
 const EmptyRestApi = require('./api/EmptyRestApi');
 const LoggedListeningServer = require('./server/LoggedListeningServer');
 
-class Backend {
+// Represented result is server(with attached api)
+class Backend extends AsyncObject {
 
   constructor(port, host, api) {
-    this.server = new LoggedListeningServer(
-      new ListeningServer(
-        new CreatedHttpServer(api || new EmptyRestApi()), port, host
-      ), `server is listening on ${host}:${port} with pid:${process.pid}`
-    );
+    super(port, host, api);
   }
 
-  run() {
-    this.server.call();
+  definedSyncCall() {
+    return (port, host, api) => {
+      let server = new LoggedListeningServer(
+        new ListeningServer(
+          new CreatedHttpServer(api || new EmptyRestApi()), port, host
+        ), `server is listening on ${host}:${port} with pid:${process.pid}`
+      );
+      server.call();
+      return server;
+    }
   }
 
 }
